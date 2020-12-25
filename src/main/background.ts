74f372ed567bec433921c04824bdc0d39cc1f0ef
setvect <setvect@naver.com>
2020-12-25 11:35:49 +0900

    코드 규칙에 맞게 변경

--- src/main/background.ts
+++ src/main/background.ts
@@ -1,6 +1,6 @@
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import { app, BrowserWindow } from 'electron'
-const isDevelopment = process.env.NODE_ENV !== 'production'
+import { createProtocol } from "vue-cli-plugin-electron-builder/lib"
+import { app, BrowserWindow } from "electron"
+const isDevelopment = process.env.NODE_ENV !== "production"
 
 app.on("ready", async () => {
   let window = new BrowserWindow({
@@ -13,8 +13,8 @@ app.on("ready", async () => {
   if (isDevelopment) {
     window.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string);
   } else {
-    createProtocol('app')
-    window.loadURL('app://./index.html')
+    createProtocol("app")
+    window.loadURL("app://./index.html")
   }
   window.on("closed", () => {
     app.quit();
